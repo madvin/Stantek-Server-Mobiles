12ebe910038efd9b70ecc34ad09fbc1b8c66a434
madvin <madvin@abv.bg>
2025-02-22 10:15:06 +0200

    add authservice for users login and register

---
+++ src/services/authService.js
@@ -0,0 +1,40 @@
+import User from '../models/User.js';
+import bcrypt from bcrypt;
+import jwt from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET || 'somesecret';
+
+export default {
+    async register(userData) {
+        
+            const userCount = await User.countDocuments({ username: userData.username });
+            if (userCount > 0) {
+                throw new Error ('Username already taken!');
+            }
+            return User.create(userData);
+        },
+    async login(userData) {
+        const user = await User.findOne({ username: userData.username});
+        
+        if (!user) {
+            throw new Error ('Invalid username or passowrd');
+        }
+
+        const isValid = await bcrypt.compare(userData.password, user.password);
+        if (!isValid) {
+            throw new Error ('Invalid username or password');
+        }
+        const payload = {
+            id: user.id,
+            username: user.username,
+        }
+        const token = jwt.sign(payload, SECRET);
+
+        return token;
+    }
+}; 
+
+
+    
+    
+
