68163af748a66bf6855d3a5fde794f87a2cddd4c
madvin <madvin@abv.bg>
2025-02-22 10:28:42 +0200

    add middleware for user authentications

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+const SECRET = process.env.JWT_SECRET || 'somesecret';
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, SECRET);
+
+        req.user = decodedToken;
+        res.locals.user = decodedToken;
+
+        next();
+    } catch (err) {
+        res.setError('Invalid Authentication!');
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
+    }
+};
+
+export const isAuth = (req, res, next) => {
+    if (!req.user) {
+        res.setError('You must be logged in to access this page!');
+        return res.redirect('/auth/login');
+    }
+    next();
+}
